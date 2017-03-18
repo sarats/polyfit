e31a7910a68d1754cde244e5bf9c6a9b8fe3c6fc
sarats <sarats@localhost>
2017-03-18 16:57:24 -0400

    enable c++ linking

--- polyfit.h
+++ polyfit.h
@@ -1,11 +1,22 @@
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
 #if !defined(POLYFIT_H)
 #define POLYFIT_H
 
+#if defined(USE_GSL)
 #include <gsl/gsl_multifit.h>
 #include <gsl/gsl_poly.h>
+#endif
+
 #include <math.h>
 
 int polyfit(int npoints, int degree, double *xi, double *yi, double *coeff);
 double polyval(double *coeff, int degree, double x);
 
+#endif
+
+#if defined(__cplusplus)
+}
 #endif
